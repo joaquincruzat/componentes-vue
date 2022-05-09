48acf152f9ca04d8ed5ee4460e23c1cf52cbc258
joaquincruzat <joaquincruzat@gmail.com>
2022-05-08 20:33:23 -0400

    desafío terminado

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1 class="title">Componentes Vue</h1>
+    <Characters 
+    v-for="(personaje, i) of personajes"
+    :key="i"
+    :nombre="personaje.nombre"
+    :imagen="personaje.src"/>
+  </div>
+</template>
+
+<script>
+import Characters from "./components/Characters.vue";
+export default {
+  name: "App",
+  components: {
+    Characters,
+  },
+  data: () => ({
+    personajes: [
+      {
+        nombre: "Rick Sanchez",
+        src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        nombre: "Morty Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      },
+      {
+        nombre: "Summer Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      },
+      {
+        nombre: "Beth Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      },
+      {
+        nombre: "Jerry Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+body {
+  background-image: url("../src/assets/img/background.png")
+}
+.title {
+  color:rgb(137, 207, 32);
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
---
+++ src/components/Characters.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list-characters">
+      <img :src="imagen">
+      <p>{{ nombre }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+props: ["nombre", "imagen"]
+}
+</script>
+
+<style>
+.list-characters {
+    display: flex;
+    margin: 20px;
+    align-items: center; 
+    }
+
+img {
+    border-radius: 50%;
+    width: 10rem;
+}
+p {
+    color: rgb(137, 207, 32);
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 20px;
+    margin: 20px;
+}
+</style>
